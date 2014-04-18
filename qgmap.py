f33eb4d995b10422d69e379def8943a94998591b
David García Garzón <voki@canvoki.net>
2014-04-19 00:11:50 +0200

    Code moved into the constructor

--- qgmap.py
+++ qgmap.py
@@ -3,29 +3,31 @@
 from PySide import QtCore, QtGui, QtWebKit
 import os
 
-
 class LoggedPage(QtWebKit.QWebPage):
-    def javaScriptConsoleMessage(self, msg, line, source):
-        print ('JS: %s line %d: %s' % (source, line, msg))
+	def javaScriptConsoleMessage(self, msg, line, source):
+		print ('JS: %s line %d: %s' % (source, line, msg))
+
 
 class QGoogleMap(QtWebKit.QWebView) :
-	def __init__(self) :
+
+	def __init__(self, debug=True) :
 		super(QGoogleMap, self).__init__()
-		QtWebKit.QWebSettings.globalSettings().setAttribute(QtWebKit.QWebSettings.DeveloperExtrasEnabled, True)
-		self.setPage(LoggedPage())
+		if debug :
+			QtWebKit.QWebSettings.globalSettings().setAttribute(
+				QtWebKit.QWebSettings.DeveloperExtrasEnabled, True)
+			self.setPage(LoggedPage())
+		basePath=os.path.abspath(os.path.dirname(__file__))
+		url = 'file://'+basePath+'/qgmap.html'
+		self.load(url)
 
-	pass
 
-
-basePath=os.path.abspath(os.path.dirname(__file__))
 
 
 if __name__ == '__main__' :
-	app = QtGui.QApplication([])
 
+
+	app = QtGui.QApplication([])
 	w = QGoogleMap()
-	url = 'file://'+basePath+'/qgmap.html'
-	w.load(url)
 	w.show()
 
 	app.exec_()
