e022ef7c31e0de5f04636fb7950565766ad4de18
David García Garzón <voki@canvoki.net>
2014-04-19 02:33:11 +0200

    Interactive example

--- qgmap.py
+++ qgmap.py
@@ -2,61 +2,87 @@
 
 from PySide import QtCore, QtGui, QtWebKit, QtNetwork
 import os
+import decorator
+
+doTrace = False
+
+@decorator.decorator
+def trace(function, *args) :
+	"""Decorates a function by tracing the begining and
+	end of the function execution, if doTrace global is True"""
+
+	if doTrace : print ("> "+function.__name__, args)
+	result = function(*args)
+	if doTrace : print ("< "+function.__name__, args, "->", result)
+	return result
 
 class LoggedPage(QtWebKit.QWebPage):
+	@trace
 	def javaScriptConsoleMessage(self, msg, line, source):
 		print ('JS: %s line %d: %s' % (source, line, msg))
 
 
 class QGoogleMap(QtWebKit.QWebView) :
 
-	def __init__(self, debug=True) :
-		super(QGoogleMap, self).__init__()
+	@trace
+	def __init__(self, parent, debug=True) :
+		super(QGoogleMap, self).__init__(parent)
 		if debug :
 			QtWebKit.QWebSettings.globalSettings().setAttribute(
 				QtWebKit.QWebSettings.DeveloperExtrasEnabled, True)
 			self.setPage(LoggedPage())
 
-		self.loadFinished.connect(self.onLoadFinished)
-
 		self.initialized = False
+		self.loadFinished.connect(self.onLoadFinished)
 
 		basePath=os.path.abspath(os.path.dirname(__file__))
 		url = 'file://'+basePath+'/qgmap.html'
 		self.load(url)
 
+	@trace
 	def waitUntilReady(self) :
-		while not w.initialized :
-			QtCore.QCoreApplication.processEvents()
+		while not self.initialized :
+			QtGui.QApplication.processEvents()
 
+	@trace
 	def onLoadFinished(self, ok) :
+		print("onLoadFinished")
 		if self.initialized : return
-		if not ok : return
+		if not ok :
+			print("Error initializing Google Maps")
 		self.initialized = True
 		self.centerAt(0,0)
 		self.setZoom(1)
-#		self.centerAt(41.35,2.05)
-#		self.setZoom(13)
 
+	@trace
 	def runScript(self, script) :
 		self.page().mainFrame().evaluateJavaScript(script)
 
 
+	@trace
 	def centerAt(self, latitude, longitude) :
 		self.runScript("setGMapCenter({},{})".format(latitude, longitude))
 
+	@trace
 	def setZoom(self, zoom) :
 		self.runScript("setGMapZoom({})".format(zoom))
 
+	@trace
 	def centerAtAddress(self, location) :
 		
 		url = QtCore.QUrl("http://maps.googleapis.com/maps/api/geocode/xml")
 		url.addQueryItem("address", location)
 		url.addQueryItem("sensor", "false")
-
+		"""
+		url = QtCore.QUrl("http://maps.google.com/maps/geo/")
+		url.addQueryItem("q", location)
+		url.addQueryItem("output", "csv")
+		url.addQueryItem("sensor", "false")
+		"""
 		request = QtNetwork.QNetworkRequest(url)
 		self._accessManager().get(request)
 
+	@trace
 	def _accessManager(self) :
 		"""Lazy initializer for the network access manager"""
 		if not hasattr(self, "_connectionManager") :
@@ -64,25 +90,20 @@ class QGoogleMap(QtWebKit.QWebView) :
 			self._connectionManager.finished.connect(self.geocodeReturned)
 		return self._connectionManager
 
+	@trace
 	def geocodeReturned(self, reply) :
 		xml = reply.readAll()
-		print (xml)
 		reader = QtCore.QXmlStreamReader(xml)
 		while not reader.atEnd() :
 			reader.readNext()
-			print ("X",reader.name())
 			if reader.name() != "geometry" : continue
-			print (reader.name())
 			reader.readNextStartElement()
 			if reader.name() != "location" : continue
-			print (reader.name())
 			reader.readNextStartElement()
 			if reader.name() != "lat" : continue
-			print (reader.name())
 			latitude = float(reader.readElementText())
 			reader.readNextStartElement()
 			if reader.name() != "lng" : continue
-			print (reader.name())
 			longitude = float(reader.readElementText())
 			self.centerAt(latitude, longitude)
 			return
@@ -90,12 +111,25 @@ class QGoogleMap(QtWebKit.QWebView) :
 
 if __name__ == '__main__' :
 
+	def go() :
+		gmap.centerAtAddress(edit.text())
+
 	app = QtGui.QApplication([])
-	w = QGoogleMap()
+	w = QtGui.QDialog()
+	gmap = QGoogleMap(w)
+	gmap.waitUntilReady()
+	edit = QtGui.QLineEdit()
+	edit.editingFinished.connect(go)
+	l = QtGui.QVBoxLayout(w)
+	l.addWidget(edit)
+	l.addWidget(gmap)
 	w.show()
-	w.waitUntilReady()
-	w.centerAtAddress("Verdaguer 40, Sant Joan Despí")
-	w.setZoom(17)
+	gmap.centerAt(41.35,2.05)
+	gmap.setZoom(13)
+	gmap.centerAtAddress("Verdaguer 40, Sant Joan Despí")
+	gmap.centerAtAddress("Maragall 1, Santa Coloma de Cervelló")
+	gmap.setZoom(17)
+	gmap.runScript("getGMapCenter()")
 
 	app.exec_()
 
